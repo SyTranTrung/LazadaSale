2337efe4571662545e359df24a223d4f55a79801
trungsy1 <trantrungsy48@gmail.com>
2017-01-11 22:24:33 +0700

    initialze

---
+++ src/lazada_sale/Lazada_Sale.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lazada_sale;
+
+/**
+ *
+ * @author Windows 7
+ */
+public class Lazada_Sale {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
